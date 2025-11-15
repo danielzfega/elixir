2fc27d2130c34c157e2152136b47e2842d0fed2d
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-11-15 23:59:41 +0100

    do intro

--- intro.exs
+++ intro.exs
@@ -1,1 +1,19 @@
 IO.puts("Hello, Elixir!")
+defmodule Intro do
+  @moduledoc """
+  A simple module to demonstrate Elixir basics.
+  """
+
+  @doc """
+  Greets the world.
+
+  ## Examples
+
+      iex> Intro.greet()
+      "Hello, World!"
+
+  """
+  def greet do
+    "Hello, World!"
+  end
+end
